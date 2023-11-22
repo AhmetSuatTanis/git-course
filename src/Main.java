86e2071be5fedcbbd72e11a6ee09050028acc71d
Ahmet Suat Tanış <ahmetsuattanis@gmail.com>
2023-11-22 21:19:26 +0300

    ahmet first branch

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        System.out.println("Şu an first Branch'in içindeyim");
-        System.out.println("Şu an first Branch'in içindeyim");
-        System.out.println("Şu an first Branch'in içindeyim");
-        System.out.println("Şu an first Branch'in içindeyim");
+        System.out.println("Kendi kendime oluşturduğum branch");
+
 
 
 
